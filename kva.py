aec5c251b7596c72ce7c6301385b74acf1bffca1
auiod <katia.zavieiboroda@mail.ru>
2022-12-18 02:45:06 +0300

    added drygie bliki

--- kva.py
+++ kva.py
@@ -26,3 +26,11 @@ def kvadratic_blik(x, y, a, i):
     pygame.draw.rect(screen, WHITE, (x+2*a/10+5, y+a/10+5, a/10, a/10))
     pygame.draw.rect(screen, WHITE, (x+a/10+5, y+2*a/10+5, a/10, a/10))
     pygame.draw.rect(screen, WHITE, (x+5, y+5, a/10, a/10))
+
+def kvadratic_bigblik(x, y, a, i):
+    ''' i - номер в массиве, который зависит от уровня'''
+    colors = COLORS[i]
+    pygame.draw.rect(screen, colors[0], (x, y, a, a))
+    pygame.draw.rect(screen, BLACK, (x, y, a, a), 5)
+    pygame.draw.rect(screen, WHITE, (x + a / 10 + 5, y + a / 10 + 5, a - a / 5 - 10, a - a / 5 - 10))
+    pygame.draw.rect(screen, WHITE, (x + 5, y + 5, a / 10, a / 10))
