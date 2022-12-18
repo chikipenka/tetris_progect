d7d3616583c923e514e3b4f9059540f151e36df2
auiod <katia.zavieiboroda@mail.ru>
2022-12-18 18:17:35 +0300

    animatia

--- kva.py
+++ kva.py
@@ -1,5 +1,16 @@
 import pygame
 from random import randint
+import sys
+from pygame.constants import QUIT, K_ESCAPE, KEYDOWN
+
+pygame.init()
+
+FPS = 100
+
+width = 1200
+height = 800
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
 
 BLUE = (65, 105, 225)
 RED = (139, 0, 0)
@@ -20,7 +31,7 @@ xx, yy = 100, 100 # вставить реальные значения
 
 def kvadratic_blik(x, y, a, i):
     ''' i - номер в массиве, который зависит от уровня
-    и x пусть меняется от xx до xx+ширина игрового окна
+    и x пусть меняется от xx до xx+ширина игрового поля
     '''
     if y>= yy:
         colors = COLORS[i]
@@ -47,3 +58,29 @@ def kvadratic_bigblik(x, y, a, i):
     pygame.draw.rect(screen, BLACK, (x, y, a, a), 5)
     pygame.draw.rect(screen, WHITE, (x + a / 10 + 5, y + a / 10 + 5, a - a / 5 - 10, a - a / 5 - 10))
     pygame.draw.rect(screen, WHITE, (x + 5, y + 5, a / 10, a / 10))
+def belii(x, y, a):
+    pygame.draw.rect(screen, WHITE, (x, y, a, a))
+
+def chornii(x, y, a):
+    pygame.draw.rect(screen, BLACK, (x, y, a, a))
+
+
+animation_set = [[kvadratic_blik(), kvadratic_bigblik()], belii(), chornii()]
+def my_animation(i):
+    animation_set = [[kvadratic_blik(), kvadratic_bigblik()], belii(), chornii()]
+    # список для хранения кадров и таймер
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((0, 0, 0))
+        j = 0
+        screen.blit(animation_set[j][i], (100, 20))
+        i += 1
+        if i == 3:
+            break
+
+        pygame.display.flip()
+        clock.tick(60)
