4d0463601a5c4f3d6428a3cda89617645265d0ea
demkef <dmitriy-nik09@mail.ru>
2022-12-18 22:47:29 +0300

    defenitions descriptions

--- playing_field.py
+++ playing_field.py
@@ -20,10 +20,10 @@ y1 = 6
 y2 = 2
 # variable texts (start value)
 score = 0
-lvl = 0
-trt = 0
-line = 0
-#Nickname
+LVL = 0
+TRT = 0
+Line = 0
+# Nickname
 print("Please enter your nickname:")
 nick_n = str(input())
 # main(first) border colour
@@ -48,36 +48,57 @@ font2 = pygame.font.SysFont("Courier New", 28, bold=True)
 
 
 def scorer_draw(scr, clr, font):
+    """"функция отрисовки очков игрока в поле score
+        функция принимает три параметра: количество очков, цвет текста и шрифт текста
+        функция выводит на экран в нужной ячейке количество очков заданым цветом и шрифтом"""
     Text = font.render(str(scr), True, clr)
     TextRect = Text.get_rect()
     TextRect.center = (290 / 405 * w, 45 / 630 * h)
     screen.blit(Text, TextRect)
 
+
 def lvlup_draw(lvl, clr, font):
+    """"функция отрисовки уровня игрока в поле lvl
+        функция принимает три параметра: уровень игрока, цвет текста и шрифт текста
+        функция выводит на экран в нужной ячейке уровень игрока заданым цветом и шрифтом"""
     Text = font.render(str(lvl), True, clr)
     TextRect = Text.get_rect()
     TextRect.center = (140 / 405 * w, 460 / 630 * h)
     screen.blit(Text, TextRect)
 
+
 def trt_draw(trt, clr, font):
+    """"функция отрисовки параметра trt в поле TRT
+        функция принимает три параметра: процент линий зачищеных тетрисом(trt), цвет текста и шрифт текста
+        функция выводит на экран в нужной ячейке показатель trt заданым цветом и шрифтом"""
     Text = font.render(str(trt), True, clr)
     TextRect = Text.get_rect()
     TextRect.center = (128 / 405 * w, 530 / 630 * h)
     screen.blit(Text, TextRect)
 
-def LINE_draw(line, clr, font):
+
+def line_draw(line, clr, font):
+    """"функция отрисовки количества линий зачищеных игроком (или тетрисом) в поле LINE
+        функция принимает три параметра: количества линий зачищеных игроком, цвет текста и шрифт текста
+        функция выводит на экран в нужной ячейке количества линий зачищеных игроком заданым цветом и шрифтом"""
     Text = font.render(str(line), True, clr)
     TextRect = Text.get_rect()
     TextRect.center = (235 / 405 * w, 105 / 630 * h)
     screen.blit(Text, TextRect)
 
-def NickName(nn, clr, font):
+
+def nick_name(nn, clr, font):
+    """"функция отрисовки никнейма в поле
+        функция принимает три параметра: никнейм, цвет текста и шрифт текста
+        функция выводит на экран в нужной ячейке никнейм заданым цветом и шрифтом"""
     Text = font.render(str(nn), True, clr)
     TextRect = Text.get_rect()
     TextRect.center = (290 / 405 * w, 595 / 630 * h)
     screen.blit(Text, TextRect)
 
+
 def drawer():
+    """"функция отрисовки игрового поля"""
     # feild
     rect(screen, clr1, (180 / 405 * w, 125 / 630 * h, 220 / 405 * w, 440 / 630 * h), 5)
     rect(screen, clr2, (182 / 405 * w, 129 / 630 * h, 216 / 405 * w, 434 / 630 * h), 2)
@@ -132,15 +153,13 @@ clock = pygame.time.Clock()
 finished = False
 
 while not finished:
-    # texts
-    # add background color using RGB values
     screen.fill((15, 10, 30))
     drawer()
     scorer_draw(score, clr3, font1)
-    lvlup_draw(lvl, clr3, font2)
-    LINE_draw(line, clr3, font1)
-    trt_draw(trt, clr3, font2)
-    NickName(nick_n, clr3, font1)
+    lvlup_draw(LVL, clr3, font2)
+    line_draw(Line, clr3, font1)
+    trt_draw(TRT, clr3, font2)
+    nick_name(nick_n, clr3, font1)
     clock.tick(FPS)
     pygame.display.update()
 
