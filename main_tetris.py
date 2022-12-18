f494359fc314d4b30ae69ed78ee2eb5ca5ab930a
auiod <katia.zavieiboroda@mail.ru>
2022-12-18 22:39:23 +0300

    added

--- main_tetris.py
+++ main_tetris.py
@@ -10,6 +10,23 @@ width = 1200
 height = 800
 screen = pygame.display.set_mode((width, height))
 
+BLUE = (65, 105, 225)
+RED = (139, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+PURPLE = (102, 11, 171)
+DPURPLE = (90, 1, 90)
+YELLOW = (219, 165, 38)
+BLUWUE = (91, 221, 230)
+GREN = (4, 115, 15)
+GREENY = (125, 223, 96)
+PINK = (245, 125, 245)
+GREEN = (0, 255, 26)
+FGREN = (35, 185, 105)
+MALINA = (185, 35, 130)
+COLORS = [[BLUE, RED], [PURPLE, DPURPLE], [RED, YELLOW], [BLUWUE, BLUE], [GREN, GREENY], [PURPLE, PINK], [BLUE, GREEN], [MALINA, FGREN]]
+xx, yy = 100, 100 # вставить реальные значения координат левого верхнего угла игрового поля
+
 types = ['square', 'left_z_figure', 'right_z_figure', 'left_l_figure', 'right_l_figure', 't_figure', 'stick']
 orientation = ['straight', 'left', 'right', 'bottom']
 
@@ -46,6 +63,27 @@ class figures:
             if self.coordinates[i][0] > 0:
                 self.coordinates[i][0] += 1
 
+
+def kvadratic_blik(x, y, a, i):
+    ''' i - номер в массиве, который зависит от уровня
+    и x пусть меняется от xx до xx+ширина игрового поля
+    y строго от yy
+    '''
+    colors = COLORS[i]
+    pygame.draw.rect(screen, colors[randint(0,1)], (x, y, a, a))
+    pygame.draw.rect(screen, BLACK, (x, y, a, a), 5)
+    pygame.draw.rect(screen, WHITE, (x+a/10+5, y+a/10+5, a/10, a/10))
+    pygame.draw.rect(screen, WHITE, (x+2*a/10+5, y+a/10+5, a/10, a/10))
+    pygame.draw.rect(screen, WHITE, (x+a/10+5, y+2*a/10+5, a/10, a/10))
+    pygame.draw.rect(screen, WHITE, (x+5, y+5, a/10, a/10))
+
+def kvadratic_bigblik(x, y, a, i):
+    ''' i - номер в массиве, который зависит от уровня'''
+    colors = COLORS[i]
+    pygame.draw.rect(screen, colors[0], (x, y, a, a))
+    pygame.draw.rect(screen, BLACK, (x, y, a, a), 5)
+    pygame.draw.rect(screen, WHITE, (x + a / 10 + 5, y + a / 10 + 5, a - a / 5 - 10, a - a / 5 - 10))
+    pygame.draw.rect(screen, WHITE, (x + 5, y + 5, a / 10, a / 10))
 
 print('Huy')
 pygame.quit()
